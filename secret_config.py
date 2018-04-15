5997d9a45f5aa7e888d61a340849789ffd662a6a
Thorin10 <34487856+Thorin10@users.noreply.github.com>
2018-04-15 18:04:13 +0200

    Update secret_config.py

--- secret_config.py
+++ secret_config.py
@@ -1,6 +1,6 @@
 #Database config
 DATABASE_HOST = 'localhost'
 DATABASE_USER = 'root'
-DATABASE_PASSWORD = 'Thomas33'
+DATABASE_PASSWORD = 'root'
 DATABASE_NAME = 'cours_iot_python_pour_le_web'
 SECRET_KEY = 'Salut'
